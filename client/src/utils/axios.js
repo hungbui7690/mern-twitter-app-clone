6b5cbe02163e4939fc3912c4aa409318a5af988f
Hung Bui <hungbui7690@gmail.com>
2024-09-14 01:10:25 -0600

    Change from dev url to prod url for axios instance

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.NODE_ENV !== 'production'
-      ? 'http://localhost:5000/api/v1'
-      : 'https://mern-twitter-app-clone.onrender.com/api/v1',
+  baseURL: 'https://mern-twitter-app-clone.onrender.com/api/v1',
 })
 
 axiosInstance.defaults.withCredentials = true
